79e190f66280c392e566e1e50902b3e85d66d5f7
Michele Mondelli <m.mondelli@zcscompany.com>
2026-09-18 10:12:15 +0200

    feat: add CI to rebuild and push images on upstream update

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+PUSH="--push"
+
 set -eux 
 
 docker buildx create --name container --driver=docker-container default || true
 
-docker buildx build --platform linux/amd64,linux/arm64 --sbom=true --provenance=true --builder=container --pull --push --target base -f Dockerfile21 -t zcscompany/java:21-base .
+docker buildx build --platform linux/amd64,linux/arm64 --sbom=true --provenance=true --builder=container --pull ${PUSH} --target base -f Dockerfile21 -t zcscompany/java:21-base .
 
-docker buildx build --platform linux/amd64,linux/arm64 --sbom=true --provenance=true --builder=container --pull --push --target dev -f Dockerfile21 -t zcscompany/java:21-dev .
+docker buildx build --platform linux/amd64,linux/arm64 --sbom=true --provenance=true --builder=container --pull ${PUSH} --target dev -f Dockerfile21 -t zcscompany/java:21-dev .
 
-docker buildx build --platform linux/amd64,linux/arm64 --sbom=true --provenance=true --builder=container --pull --push --target dist -f Dockerfile21 -t zcscompany/java:21-dist .
+docker buildx build --platform linux/amd64,linux/arm64 --sbom=true --provenance=true --builder=container --pull ${PUSH} --target dist -f Dockerfile21 -t zcscompany/java:21-dist .
 
-docker buildx build --platform linux/amd64,linux/arm64 --sbom=true --provenance=true --builder=container --pull --push --target base -f Dockerfile17 -t zcscompany/java:17-base .
+docker buildx build --platform linux/amd64,linux/arm64 --sbom=true --provenance=true --builder=container --pull ${PUSH} --target base -f Dockerfile17 -t zcscompany/java:17-base .
 
-docker buildx build --platform linux/amd64,linux/arm64 --sbom=true --provenance=true --builder=container --pull --push --target dev -f Dockerfile17 -t zcscompany/java:17-dev .
+docker buildx build --platform linux/amd64,linux/arm64 --sbom=true --provenance=true --builder=container --pull ${PUSH} --target dev -f Dockerfile17 -t zcscompany/java:17-dev .
 
-docker buildx build --platform linux/amd64,linux/arm64 --sbom=true --provenance=true --builder=container --pull --push --target dist -f Dockerfile17 -t zcscompany/java:17-dist .
+docker buildx build --platform linux/amd64,linux/arm64 --sbom=true --provenance=true --builder=container --pull ${PUSH} --target dist -f Dockerfile17 -t zcscompany/java:17-dist .
 
 docker buildx stop container
